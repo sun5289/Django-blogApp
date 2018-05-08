b2b720eb24670a02665d481a4a63c611c185cbdc
sun5289 <sun5289@naver.com>
2018-05-08 16:02:54 +0900

    Djanog Blog App

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')   # 글작성자
+    title = models.CharField(max_length=255)    # 제목
+    text = models.TextField()                  # 글 내용
+    created_date = models.DateTimeField(default=timezone.now)  # 글작성일자
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return  self.title
---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from blog import views
+
+urlpatterns = [
+
+    url(r'^$',views.post_list,name='post_list'),
+]
